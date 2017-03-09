4c4d0bfa5417098f7b9534244482353095a87bad
Sishuai <919976766@qq.com>
2017-03-09 15:13:27 +0800

    推

--- FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/ChaseFragment.java
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/ChaseFragment.java
@@ -20,19 +20,24 @@ package lanou.com.fakebilibili.fragment;
  * 　 ▊　▂　▊　　　　　　▊　▂　▊
  **/
 
+import android.support.v7.widget.RecyclerView;
+
+import lanou.com.fakebilibili.R;
+
 /**
  * Created by 司帅 on 17/3/9.
  */
 
 public class ChaseFragment extends BaseFragment{
+    private RecyclerView chaseRecyclerView;
     @Override
     protected int bindLayout() {
-        return 0;
+        return R.layout.fragment_chase;
     }
 
     @Override
     protected void initView() {
-
+        chaseRecyclerView = bindView(R.id.chase_recyclerView);
     }
 
     @Override
