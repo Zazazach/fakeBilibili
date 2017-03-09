b5252bbcc9ba5c39a5be86415d10def40af6d75a
Sishuai <919976766@qq.com>
2017-03-09 14:45:07 +0800

    333

---
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/ChaseFragment.java
@@ -0,0 +1,47 @@
+package lanou.com.fakebilibili.fragment;
+/**
+ * ██████齐天大圣 - 司帅████████
+ *
+ * 　　 ◢████████████████◣
+ * 　　██　　　 ◥██◤　　　 ██
+ * 　◢███　　　　◥◤　　　  ██◣
+ * 　▊▎██◣　　　　　　　　 ◢█▊▊
+ * 　▊▎██◤　　●　 　●　   ◥█▊ ▊
+ * 　▊ ██　　　　　　　　　 █▊▊
+ * 　◥▇██　▊　　　　　　▊　 █▇◤
+ * 　　◥█　   ◥▆▄▄▆◤　    █◤　　　    ◢▇▇◣
+ * ◢████◥◥▆▅▄▂▂▂▂▂▂▂▄▅▅▆▆█████◣　   ▊◢　█
+ * █╳　　　　　　　　　　　　　　╳█　   ◥◤◢◤
+ * ◥█◣　　　˙　　　　　˙　　　◢█◤　　   ◢◤
+ * 　　▊　　　　　　　　　　　　▊　　　　█
+ * 　　▊　　　　　　　　　　　　▊　　　◢◤
+ * 　　▊　　　　　⊕　 　　　 　█▇▇▇◤
+ * ◢█▇▆▆▆▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▆▆▆▇█◣
+ * 　 ▊　▂　▊　　　　　　▊　▂　▊
+ **/
+
+/**
+ * Created by 司帅 on 17/3/9.
+ */
+
+public class ChaseFragment extends BaseFragment{
+    @Override
+    protected int bindLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void bindEvent() {
+
+    }
+}
