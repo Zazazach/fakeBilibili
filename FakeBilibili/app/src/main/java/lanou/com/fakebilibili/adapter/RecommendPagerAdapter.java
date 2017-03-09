4e203a2e4e8103e7d7cd9b858a1c53bd9c9fd388
Parselable <605113281@qq.com>
2017-03-09 15:40:00 +0800

    完成推荐页分页功能 待检测

--- FakeBilibili/app/src/main/java/lanou/com/fakebilibili/adapter/RecommendPagerAdapter.java
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/adapter/RecommendPagerAdapter.java
@@ -12,9 +12,11 @@ import java.util.List;
 
 public class RecommendPagerAdapter extends FragmentPagerAdapter {
     private List<Fragment> fragments;
+    private String[] titles = {"综合","动态"};
 
     public void setFragments(List<Fragment> fragments) {
         this.fragments = fragments;
+        notifyDataSetChanged();
     }
 
     public RecommendPagerAdapter(FragmentManager fm) {
@@ -29,5 +31,8 @@ public class RecommendPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return fragments != null ? fragments.size() : 0;
+    }
+    public String getTitles(int position){
+        return titles[position];
     }
 }
