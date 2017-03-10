95fa384ad1e6c4652143a25276cae9fed7fc4990
Sishuai <919976766@qq.com>
2017-03-10 17:08:18 +0800

    加入点击动画

---
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/utils/animatorutils/AnimatorUtils.java
@@ -0,0 +1,42 @@
+package lanou.com.fakebilibili.utils.animatorutils;
+/**
+ * ██████齐天大圣 - 司帅████████
+ *
+ * 　　 ◢████████████████◣
+ * 　　██　　　 ◥██◤　　　 ██
+ * 　◢███　　　　◥◤　　　  ██◣
+ * 　▊▎██◣　　　　　　　　 ◢█▊▊
+ * 　▊▎██◤　　●　 　●　   ◥█▊ ▊
+ * 　▊ ██　　　　　　　　　 █▊▊
+ * 　◥▇██　▊　　　　　　▊　 █▇◤
+ * 　　◥█　   ◥▆▄▄▆◤　    █◤　　　    ◢▇▇◣
+ * ◢████◥◥▆▅▄▂▂▂▂▂▂▂▄▅▅▆▆█████◣　   ▊◢　█
+ * █╳　　　　　　　　　　　　　　╳█　   ◥◤◢◤
+ * ◥█◣　　　˙　　　　　˙　　　◢█◤　　   ◢◤
+ * 　　▊　　　　　　　　　　　　▊　　　　█
+ * 　　▊　　　　　　　　　　　　▊　　　◢◤
+ * 　　▊　　　　　⊕　 　　　 　█▇▇▇◤
+ * ◢█▇▆▆▆▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▆▆▆▇█◣
+ * 　 ▊　▂　▊　　　　　　▊　▂　▊
+ **/
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.view.View;
+
+/**
+ * Created by 司帅 on 17/3/10.
+ */
+
+public class AnimatorUtils {
+    public static void setObjectAnimator(View view) {
+        JellyInterpolator myInterpolator = new JellyInterpolator();
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(
+                ObjectAnimator.ofFloat(view, "scaleX",0f,1f)
+                ,ObjectAnimator.ofFloat(view, "scaleY",0f,1f));
+        animatorSet.setInterpolator(myInterpolator);
+        animatorSet.setDuration(2500);
+        animatorSet.start();
+    }
+}
---
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/utils/animatorutils/JellyInterpolator.java
@@ -0,0 +1,35 @@
+package lanou.com.fakebilibili.utils.animatorutils;
+/**
+ * ██████齐天大圣 - 司帅████████
+ *
+ * 　　 ◢████████████████◣
+ * 　　██　　　 ◥██◤　　　 ██
+ * 　◢███　　　　◥◤　　　  ██◣
+ * 　▊▎██◣　　　　　　　　 ◢█▊▊
+ * 　▊▎██◤　　●　 　●　   ◥█▊ ▊
+ * 　▊ ██　　　　　　　　　 █▊▊
+ * 　◥▇██　▊　　　　　　▊　 █▇◤
+ * 　　◥█　   ◥▆▄▄▆◤　    █◤　　　    ◢▇▇◣
+ * ◢████◥◥▆▅▄▂▂▂▂▂▂▂▄▅▅▆▆█████◣　   ▊◢　█
+ * █╳　　　　　　　　　　　　　　╳█　   ◥◤◢◤
+ * ◥█◣　　　˙　　　　　˙　　　◢█◤　　   ◢◤
+ * 　　▊　　　　　　　　　　　　▊　　　　█
+ * 　　▊　　　　　　　　　　　　▊　　　◢◤
+ * 　　▊　　　　　⊕　 　　　 　█▇▇▇◤
+ * ◢█▇▆▆▆▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▆▆▆▇█◣
+ * 　 ▊　▂　▊　　　　　　▊　▂　▊
+ **/
+
+import android.view.animation.LinearInterpolator;
+
+/**
+ * Created by 司帅 on 17/3/10.
+ */
+
+public class JellyInterpolator extends LinearInterpolator{
+    private float factor = 0.2f;
+    @Override
+    public float getInterpolation(float input) {
+        return (float) (Math.pow(2, -10 * input) * Math.sin((input - factor / 4) * (2 * Math.PI) / factor) + 1);
+    }
+}
