b53bedad5323d2f643ce6399207daf685f1b50d5
Parselable <605113281@qq.com>
2017-03-10 15:16:41 +0800

    no message

--- FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/SynthesizeFragment.java
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/SynthesizeFragment.java
@@ -1,12 +1,17 @@
 package lanou.com.fakebilibili.fragment;
 
 import lanou.com.fakebilibili.R;
+import lanou.com.fakebilibili.ijk.IjkVideoView;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 /**
  * Created by Parcelable on 17/3/9.
  */
 
 public class SynthesizeFragment extends BaseFragment {
+    private IjkVideoView videoView;
+    private String url = "http://cn-tj4-cu.acgvideo.com/vg3/c/06/14772544-1-hd.mp4?expires=1489122600&platform=android&ssig=Di3hDHOmByow2dn2uo9Ccw&oi=2056146356&nfa=RGeBDJQwd8EMY10XEydOtw==&dynamic=1";
+
     @Override
     protected int bindLayout() {
         return R.layout.fragment_synthesize;
@@ -14,11 +19,18 @@ public class SynthesizeFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        videoView = bindView(R.id.video_view);
 
     }
 
     @Override
     protected void initData() {
+
+        videoView.setVideoPath(url);
+        videoView.start();
+
+
+
 
     }
 
@@ -26,4 +38,5 @@ public class SynthesizeFragment extends BaseFragment {
     protected void bindEvent() {
 
     }
+
 }
