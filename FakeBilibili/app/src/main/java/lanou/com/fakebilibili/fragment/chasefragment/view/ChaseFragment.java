7679a2dcec44c2c854986c94b8c7e017562f7782
Parselable <605113281@qq.com>
2017-03-10 17:48:34 +0800

    no message

--- FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/chasefragment/view/ChaseFragment.java
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/chasefragment/view/ChaseFragment.java
@@ -24,22 +24,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import lanou.com.fakebilibili.R;
-<<<<<<< HEAD:FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/ChaseFragment.java
 import lanou.com.fakebilibili.utils.BaseFragment;
 import lanou.com.fakebilibili.adapter.ChaseRecyclerViewAdapter;
-=======
-<<<<<<< HEAD:FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/ChaseFragment.java
-import lanou.com.fakebilibili.utils.BaseFragment;
-import lanou.com.fakebilibili.adapter.ChaseRecyclerViewAdapter;
-=======
-
-import lanou.com.fakebilibili.utils.BaseFragment;
-
-import lanou.com.fakebilibili.adapter.ChaseRecyclerViewAdapter;
 
 
->>>>>>> 0aab56aebcfbf903f47c54cef21a3db7d76a3d1c:FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/chasefragment/view/ChaseFragment.java
->>>>>>> 9b4be5e632a2f55b04977208ab36298964fa1bb0:FakeBilibili/app/src/main/java/lanou/com/fakebilibili/fragment/chasefragment/view/ChaseFragment.java
 
 /**
  * Created by 司帅 on 17/3/9.
