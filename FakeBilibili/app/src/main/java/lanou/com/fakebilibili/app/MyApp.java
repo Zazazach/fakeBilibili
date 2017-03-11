084e8ac42b3110ee3670be055be811b3a1c275f8
Sishuai <919976766@qq.com>
2017-03-11 14:57:11 +0800

    03111457

--- FakeBilibili/app/src/main/java/lanou/com/fakebilibili/app/MyApp.java
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/app/MyApp.java
@@ -1,4 +1,4 @@
-package lanou.com.fakebilibili.myapp;
+package lanou.com.fakebilibili.app;
 /**
  * ██████齐天大圣 - 司帅████████
  *
@@ -20,9 +20,22 @@ package lanou.com.fakebilibili.myapp;
  * 　 ▊　▂　▊　　　　　　▊　▂　▊
  **/
 
+import android.app.Application;
+import android.content.Context;
+
 /**
  * Created by 司帅 on 17/3/11.
  */
 
-public class MyApp {
+public class MyApp extends Application{
+    private static Context context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
 }
