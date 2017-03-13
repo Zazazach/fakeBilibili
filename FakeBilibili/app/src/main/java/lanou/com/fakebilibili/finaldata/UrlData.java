002f63e3b6b745fa4f49c98c6aa035323d14d19d
Parselable <605113281@qq.com>
2017-03-13 20:53:50 +0800

    no message

--- FakeBilibili/app/src/main/java/lanou/com/fakebilibili/finaldata/UrlData.java
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/finaldata/UrlData.java
@@ -17,5 +17,5 @@ public class UrlData {
     //推荐页
     public static final String RECOMMEND = "http://app.bilibili.com/x/feed/index?appkey=1d8b6e7d45233436&build=500000&idx=1488849732&mobi_app=android&network=wifi&platform=android&pull=true&style=2&ts=1488802824000&sign=7e4ff55cb86ecdceb48d631a353af5ef";
     //推荐页二级简介页面
-    public static final String RECOMMEND_SHORT = "http://app.bilibili.com/playurl?device=android&otype=json&build=500000&buvid=9ECF7B13-7522-4484-AD5D-1CBE3318452618594infoc&cid=14772544&appkey=iVGUTjsxvpLeuDCf&quality=2&platform=android&sign=dd9237026459074cbf12e5f50ca75768";
+    public static final String RECOMMEND_SHORT = "http://app.bilibili.com/playurl?device=android&mid=95403745&platform=android&cid=14913077&build=500000&appkey=iVGUTjsxvpLeuDCf&access_key=295975204b261f22960b940ccce36744&buvid=9ECF7B13-7522-4484-AD5D-1CBE3318452618594infoc&otype=json&quality=2&sign=f4f1da38c642c254f54befa4307c04bf";
 }
