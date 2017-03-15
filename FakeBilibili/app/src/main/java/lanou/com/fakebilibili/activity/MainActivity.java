87d03ba3c304d392d8588fdd8f981a9da0d04638
Parselable <605113281@qq.com>
2017-03-15 19:14:21 +0800

    no message

--- FakeBilibili/app/src/main/java/lanou/com/fakebilibili/activity/MainActivity.java
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package lanou.com.fakebilibili.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -21,6 +22,8 @@ import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.jaeger.library.StatusBarUtil;
+import com.uuzuche.lib_zxing.activity.CaptureActivity;
+import com.uuzuche.lib_zxing.activity.CodeUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +45,8 @@ public class MainActivity extends BaseActivity {
     private ViewPager viewPager;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+
+    private final int REQUEST_CODE  = 5;
 
 
     private TextView loginNavTv;
@@ -112,6 +117,17 @@ public class MainActivity extends BaseActivity {
     //对popUpWindow进行一些初始化的设置
     protected void initPopUpWindow() {
         View popView = LayoutInflater.from(this).inflate(R.layout.pop_search_home_page, null);
+
+        ImageView qrcodeIv = (ImageView) popView.findViewById(R.id.iv_qr_code_search_home_page);
+        //扫描二维码
+        qrcodeIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
         popupWindow = new PopupWindow(popView, 1300, 130, true);
         popupWindow.setAnimationStyle(R.style.AnimationFade);
         popView.setOnTouchListener(new View.OnTouchListener() {
@@ -189,11 +205,29 @@ public class MainActivity extends BaseActivity {
                 //点击显示搜索栏
                 getPopUpWindow();
 
-                popupWindow.showAtLocation(findViewById(R.id.lay_out_pop_home_page), Gravity.RIGHT, 0, -600);
+                popupWindow.showAtLocation(findViewById(R.id.lay_out_pop_home_page), Gravity.RIGHT, 0, -750);
 
             }
         });
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            //处理扫描结果（在界面上显示）
+            if (null != data) {
+                Bundle bundle = data.getExtras();
+                if (bundle == null) {
+                    return;
+                }
+                if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_SUCCESS) {
+                    String result = bundle.getString(CodeUtils.RESULT_STRING);
+                    Toast.makeText(this, "解析结果:" + result, Toast.LENGTH_LONG).show();
+                } else if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_FAILED) {
+                    Toast.makeText(MainActivity.this, "解析二维码失败", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
 }
