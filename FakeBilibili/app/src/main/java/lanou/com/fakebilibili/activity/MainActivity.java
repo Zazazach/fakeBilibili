f33c8d13274cc3442f77f8b1bf3ff23679c91cff
Sishuai <919976766@qq.com>
2017-03-18 10:37:19 +0800

    0318 10:37

--- FakeBilibili/app/src/main/java/lanou/com/fakebilibili/activity/MainActivity.java
+++ FakeBilibili/app/src/main/java/lanou/com/fakebilibili/activity/MainActivity.java
@@ -54,51 +54,27 @@ public class MainActivity extends BaseActivity {
     private ViewPager viewPager;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
-<<<<<<< HEAD
-    private final int REQUEST_CODE  = 5;
 
-=======
-<<<<<<< HEAD
-    private final int REQUEST_CODE  = 5;
-=======
-<<<<<<< HEAD
 
-    private final int REQUEST_CODE  = 5;
 
-=======
 
 
     private final int REQUEST_CODE  = 5;
 
 
 
->>>>>>> 091d3aafdbe0697682cf0da01516028b24b623d1
->>>>>>> 43e006463cd80f08d3499f4b7da82221669a81a6
->>>>>>> bd4e0d55cb9d734401a863fa49ccb37d7fa0191d
     private TextView loginNavTv;
     private ImageView loginNavIv, changeThemeNavIv, searchIv;
     private boolean isNight = false;
     private PopupWindow popupWindow;
-<<<<<<< HEAD
-=======
 
->>>>>>> 43e006463cd80f08d3499f4b7da82221669a81a6
+
     private WindowManager.LayoutParams lp;
     private AreaFragment areaFragment;
     private WindowManager windowManager;
     private View floatView;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 43e006463cd80f08d3499f4b7da82221669a81a6
 
-
->>>>>>> 091d3aafdbe0697682cf0da01516028b24b623d1
->>>>>>> bd4e0d55cb9d734401a863fa49ccb37d7fa0191d
     @Override
     public int bindLayout() {
         if (MyApp.appConfig.isNighTheme()) {
@@ -138,15 +114,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initData() {
-<<<<<<< HEAD
-
-        lp = getWindow().getAttributes();
-=======
-
 
         lp = getWindow().getAttributes();
 
->>>>>>> 43e006463cd80f08d3499f4b7da82221669a81a6
+
 
         adapter = new FragmentAdapter(getSupportFragmentManager());
         list = new ArrayList<>();
@@ -260,11 +231,7 @@ public class MainActivity extends BaseActivity {
         });
     }
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 43e006463cd80f08d3499f4b7da82221669a81a6
     //windowmanager退出
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -278,15 +245,7 @@ public class MainActivity extends BaseActivity {
 
         return super.onKeyDown(keyCode, event);
     }
-<<<<<<< HEAD
-=======
 
-
-
-
-
-
->>>>>>> 43e006463cd80f08d3499f4b7da82221669a81a6
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -306,8 +265,5 @@ public class MainActivity extends BaseActivity {
             }
         }
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> 43e006463cd80f08d3499f4b7da82221669a81a6
 }
